72ee950fea7ebe875a9190af7244291f75843ebc
Sheng Wei <shengweichow@gmail.com>
2019-12-01 17:32:24 +0800

    Typing updates

--- 2019/day1.py
+++ 2019/day1.py
@@ -1,29 +1,37 @@
 import math
 
+from typing import Callable
+
 def fuel_required(mass: int):
     return math.floor(mass / 3) - 2
 
-def fuel_required_until_empty(mass: int):
+def fuel_required_until_empty(fn: Callable[[int], int], mass: int):
     new_mass = mass
     fuel_needed = 0
     while True:
-        new_mass = fuel_required(new_mass)
+        new_mass = fn(new_mass)
         if new_mass < 0:
             break
         fuel_needed += new_mass
 
     return fuel_needed
 
+def fuel_required_nofloor(mass: int):
+    return (mass // 3) - 2
+
 def main():
     with open('2019/day1_input.txt', 'r') as f:
         lines = f.readlines()
-        fuel_requirements = [fuel_required_until_empty(int(x)) for x in lines]
 
-        total = 0
-        for r in fuel_requirements:
-            total += r
+    fuel_requirements = [
+        fuel_required_until_empty(fuel_required_nofloor, int(x))
+        for x in lines]
 
-        print(total)
+    total = 0
+    for r in fuel_requirements:
+        total += r
+
+    print(total)
 
 if __name__ == '__main__':
     main()
--- 2019/day1_test.py
+++ 2019/day1_test.py
@@ -1,14 +1,16 @@
 import unittest
-
-from day1 import fuel_required, fuel_required_until_empty
+from day1 import fuel_required, fuel_required_until_empty, fuel_required_nofloor
 
 class Day1Test(unittest.TestCase):
     def test_stage1(self):
         self.assertEqual(fuel_required(14), 2)
 
     def test_stage2(self):
-        self.assertEqual(fuel_required_until_empty(14), 2)
-        self.assertEqual(fuel_required_until_empty(1969), 966)
+        self.assertEqual(fuel_required_until_empty(fuel_required, 14), 2)
+        self.assertEqual(fuel_required_until_empty(fuel_required, 1969), 966)
+
+        self.assertEqual(fuel_required_until_empty(fuel_required_nofloor, 14), 2)
+        self.assertEqual(fuel_required_until_empty(fuel_required_nofloor, 1969), 966)
 
 if __name__ == '__main__':
     unittest.main()
