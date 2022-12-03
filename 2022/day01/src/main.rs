79f1902063c614bec11c90e83098254bb4503016
jaxsax <jaxsax@users.noreply.github.com>
2022-12-03 17:43:12 +0800

    day 1

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+    let split = input.split("\n");
+    let mut elf_index = 0;
+    let mut calories_to_elf_id = vec![0];
+    for s in split {
+        if s == "" {
+            elf_index += 1;
+            calories_to_elf_id.push(0);
+            continue;
+        }
+
+        let calories = s.parse::<i32>().unwrap();
+        calories_to_elf_id[elf_index] += calories
+    }
+
+    calories_to_elf_id.sort_by(|a, b| b.cmp(a));
+    println!("1: {}", calories_to_elf_id[0]);
+    println!("2: {}", calories_to_elf_id[1]);
+    println!("3: {}", calories_to_elf_id[2]);
+    println!(
+        "total: {}",
+        calories_to_elf_id[0] + calories_to_elf_id[1] + calories_to_elf_id[2]
+    );
 }
